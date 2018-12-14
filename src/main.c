974d5c303b42780cd64cb5bd24f506211d511c83
Vincent Ribot <vincent.ribot@free.fr>
2018-12-14 16:22:19 +0100

    fix:addRowFromTable feature:readBits

--- src/main.c
+++ src/main.c
@@ -15,11 +15,14 @@ int main(int argc, char *argv[]) {
 
 }
 
-int addRowFromTable(long long int * C, int indexRowC, long long int * T, int indexRowT) {
+addRowFromTable(long long int * C, int indexRowC, long long int * T, int indexRowT) {
     long long int * rowC = C + indexRowC*sizeRow;
     long long int * rowT = T + indexRowT*sizeRow;
-    for(int j=0; j<sizeRow/sizeof(long long int);j++){
+    for(int j=0; j<sizeRow/(sizeof(long long int)*8);j++){
         rowC[j] ^= rowT[j];
     }
 }
 
+long long int * readBits(long long int * A, int indexRow, int indexColumns, int k) {
+    return Atemp = A + indexRow*sizeRow + indexColumns;
+}
