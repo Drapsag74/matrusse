23fa95d211f82301889f8a41ee0d1d4775fc15e6
Gaspard <gaspard7426@gmail.com>
2018-12-14 15:21:41 +0100

    feat: add row from table T to C

--- src/main.c
+++ src/main.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N = 1000
+
 int main(int argc, char *argv[]) {
 
-    if (argc < 1) {
-        printf(" put matrix size");
-    }
-    int matrix[n]
+    //if (argc < 1) {
+    //    printf(" put matrix size");
+    //}
+
+    printf("%llu",N);
 
     return 0;
 
+}
+
+int addRowFromTable(long long int * C,int row1, int row2) {
+    for (int i = 0; i < N; i++) {
+
+    }
 }
