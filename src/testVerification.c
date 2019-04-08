0766bd5b0ceba98a03569651e6e4ac93e7be4383
neiloperrin-ganier <neilo.perrin-ganier@orange.fr>
2019-04-08 15:14:56 +0200

    Add:testMonteCarlo

--- src/testVerification.c
+++ src/testVerification.c
@@ -23,8 +23,8 @@ int64_t testMonteCarlo(matrix_t * A, matrix_t * B, matrix_t * M,int64_t a) {
     int64_t b = 1;
     for(int64_t i = 0; i<a; i++){
         int64_t s = 0;
-        int64_t r1 = (getpid() + rand())%(A->m);
-        int64_t r2 = (getpid() + rand())%(A->m);
+        int64_t r1 = srand(getpid())%(A->m);
+        int64_t r2 = srand(getppid())%(A->m);
         for(int64_t j = 0; j<(A->n); j++){
             s = s + (readInt64_t(A,r1,j)*readInt64_t(B,j,r2));
         }
