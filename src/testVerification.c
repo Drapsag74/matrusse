d8de3b5c5a966b3e87cdf0ff345204ef46bc688a
neiloperrin-ganier <neilo.perrin-ganier@orange.fr>
2019-04-08 15:12:16 +0200

    Add:testMonteCarlo

--- src/testVerification.c
+++ src/testVerification.c
@@ -1,5 +1,7 @@
 #include <stdint.h>
 #include "utils.h"
+#include "unistd.h"
+
 
 int64_t testDiag(matrix_t * A, matrix_t * B, matrix_t * M){
     int8_t b = 1;
@@ -21,8 +23,8 @@ int64_t testMonteCarlo(matrix_t * A, matrix_t * B, matrix_t * M,int64_t a) {
     int64_t b = 1;
     for(int64_t i = 0; i<a; i++){
         int64_t s = 0;
-        int64_t r1 = rand();
-        int64_t r2 = rand();
+        int64_t r1 = (getpid() + rand())%(A->m);
+        int64_t r2 = (getpid() + rand())%(A->m);
         for(int64_t j = 0; j<(A->n); j++){
             s = s + (readInt64_t(A,r1,j)*readInt64_t(B,j,r2));
         }
