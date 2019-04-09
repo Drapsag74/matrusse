8152cd2b170d071d9f058a2bd4d54cd1da27446a
neiloperrin-ganier <neilo.perrin-ganier@orange.fr>
2019-04-09 11:03:45 +0200

    create:benchmark

---
+++ src/benchmark.h
@@ -0,0 +1,11 @@
+//
+// Created by Neïlo PERRIN-GANIER on 2019-04-09.
+//
+
+#ifndef MATRUSSE_BENCHMARK_H
+#define MATRUSSE_BENCHMARK_H
+
+#include <python2.7/Python.h>
+
+
+#endif //MATRUSSE_BENCHMARK_H
