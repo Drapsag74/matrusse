b8e5091093d27d5a8fd7a9598b9d968bb874341f
Vicsene <senergues.victor@gmail.com>
2019-04-11 16:50:45 +0200

    Tests : -matrice null/read int 64

--- src/tests/tests-matrix.cpp
+++ src/tests/tests-matrix.cpp
@@ -20,7 +20,31 @@ REQUIRE( Factorial(3) == 6 );
 REQUIRE( Factorial(10) == 3628800 );
 }
 
-TEST_CASE( "Matrix", "[matrix]" ) {
-        matrix_t * M = nullMatrix(1000, 1000);
-        REQUIRE(extract(M, 0, 0, 1) == 1);
+TEST_CASE( "Matrix nulle puissance 2", "[matrix]" ) {
+        matrix_t * M = nullMatrix(512, 512);
+        for(int i=0;i<(512*M->nbColonneInt);i++) {
+            REQUIRE(M->value[i] == 0);
+        }
+            REQUIRE(M->m==512);
+            REQUIRE(M->n==512);
+            REQUIRE(M->nbColonneInt==8);
 }
+
+TEST_CASE("Showmatrix","[matrice]"){
+    matrix_t * M = nullMatrix(4,4);
+    for(int i=0;i<(4*M->nbColonneInt);i++) {
+        if(i%2==1){
+            M->value[i]=64;
+        }
+    }
+   // showMatrix(M);
+}
+
+TEST_CASE("readInt","[matrice]"){
+    matrix_t * M = nullMatrix(64, 128);
+    writeInt64_t(M,22,1,48);
+    writeInt64_t(M,60,0,84);
+    CHECK(readInt64_t(M,22,1)==48);
+    CHECK(readInt64_t(M,60,0)==84);
+}
+
