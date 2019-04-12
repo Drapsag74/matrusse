351186abf8c6ff7aa6d1b3b03ec5ed056a54522d
Vicsene <senergues.victor@gmail.com>
2019-04-12 14:57:32 +0200

    TEST VicToR

--- src/tests/tests-matrix.cpp
+++ src/tests/tests-matrix.cpp
@@ -1,24 +1,17 @@
 //
 // Created by typs on 11/04/19.
 //
+#include <sstream>
+#include <iostream>
+#include <immintrin.h>
+#include <emmintrin.h>
 #include "catch.hpp"
 
 extern "C" {
     #include "../matrix.h"
 }
 
-
-unsigned int Factorial( unsigned int number ) {
-    return number <= 1 ? number : Factorial(number-1)*number;
-}
-
-
-TEST_CASE( "Factorials are computed", "[factorial]" ) {
-REQUIRE( Factorial(1) == 1 );
-REQUIRE( Factorial(2) == 2 );
-REQUIRE( Factorial(3) == 6 );
-REQUIRE( Factorial(10) == 3628800 );
-}
+using namespace std;
 
 TEST_CASE( "Matrix nulle puissance 2", "[matrix]" ) {
         matrix_t * M = nullMatrix(512, 512);
@@ -30,21 +23,59 @@ TEST_CASE( "Matrix nulle puissance 2", "[matrix]" ) {
             REQUIRE(M->nbColonneInt==8);
 }
 
-TEST_CASE("Showmatrix","[matrice]"){
+/* TEST_CASE("Showmatrix","[matrice]"){
     matrix_t * M = nullMatrix(4,4);
     for(int i=0;i<(4*M->nbColonneInt);i++) {
         if(i%2==1){
             M->value[i]=64;
         }
     }
-   // showMatrix(M);
-}
+   stringstream test;
 
-TEST_CASE("readInt","[matrice]"){
-    matrix_t * M = nullMatrix(64, 128);
+    showMatrix(M);
+    test << std::cout;
+    std::cout << test;
+} */
+
+TEST_CASE("read/write Int","[matrice]"){
+    matrix_t * M = nullMatrix(64, 256);
     writeInt64_t(M,22,1,48);
     writeInt64_t(M,60,0,84);
+    __m128i a = _mm_set_epi64x(45,14);
+    __m256i b = _mm256_set_epi64x(4,6,9,11);
+    writeInt128i(M,18,1,a);
+    writeInt256i(M,25,0,b);
     CHECK(readInt64_t(M,22,1)==48);
     CHECK(readInt64_t(M,60,0)==84);
+    CHECK(readInt128i(M,18,1)[0]==14);
+    CHECK(readInt128i(M,18,1)[1]==45);
+    CHECK(readInt256i(M,25,0)[0]==11);
+    CHECK(readInt256i(M,25,0)[1]==9);
+    CHECK(readInt256i(M,25,0)[2]==6);
+    CHECK(readInt256i(M,25,0)[3]==4);
 }
+
+TEST_CASE("nbrow/column","[matrice]"){
+    matrix_t * M = aleaMatrixBinaire(512, 512);
+    matrix_t * N = nullMatrix(66, 66);
+    int64_t *c = getRow(M,2);
+    CHECK(getNbRow(M)==512);
+    CHECK(getNbColumn(M)==(512/64));
+    CHECK(getNbRow(N)==66);
+    CHECK(getNbColumn(N)==(2));
+    CHECK(c[0]==readInt64_t(M,2,0));
+}
+
+TEST_CASE("Getblock","[matrice]"){
+    matrix_t * M = aleaMatrixBinaire(512,512);
+    matrix_t * N = getBloc(M,10,20);
+    REQUIRE(getNbRow(N)==11);
+    for(int i = 0; i<getNbRow(N);i++){
+        int64_t *a = getRow(M,10+i);
+        int64_t *b = getRow(N,i);
+        CHECK(a[0]==b[0]);
+    }
+}
+
+
 
