a2a4fecbf97828af046d682b79f3fa7c6f15982f
Vicsene <senergues.victor@gmail.com>
2019-04-30 15:24:56 +0200

    Tests: add files to cmakeList and tests v1 V2

--- src/testVerification.c
+++ src/testVerification.c
@@ -3,6 +3,7 @@
 #include "unistd.h"
 #include "time.h"
 #include "testVerification.h"
+#include "utils.h"
 
 /**
  * /
@@ -37,11 +38,10 @@ int64_t testDiag(matrix_t * A, matrix_t * B, matrix_t * M){
  */
 int64_t testMonteCarlo(matrix_t * A, matrix_t * B, matrix_t * M,int64_t a) {
     int64_t b = 1;
-    srand(time(NULL));
     for(int64_t i = 0; i<a; i++){
         int64_t s = 0;
-        int64_t r1 = rand()%(A->m);
-        int64_t r2 = rand()%(A->m);
+        int64_t r1 = random_64()%(A->m);
+        int64_t r2 = random_64()%(A->m);
         for(int64_t j = 0; j<(A->n); j++){
             s += (extract(A,r1,j,1)*extract(B,j,r2,1));
         }
